b89fb291d9b64151d1ec83fa45f20f3168b2c46b
Vadim Kochetihin <vadvladko@mail.ru>
2020-01-30 22:45:17 +0300

    добавлен файл с результатом

---
+++ rezult.py
@@ -0,0 +1,37 @@
+[
+  {
+    "name": "Бренд одежды | MEDICINE",
+    "gid": 134709480,
+    "members_count": 4408
+  },
+  {
+    "name": "Новые Альбомы",
+    "gid": 4100014,
+    "members_count": 548440
+  },
+  {
+    "name": "Другая Музыка",
+    "gid": 35486626,
+    "members_count": 873025
+  },
+  {
+    "name": "E:\\music\\leftfield",
+    "gid": 101522128,
+    "members_count": 13659
+  },
+  {
+    "name": "Synthpop [Futurepop, Retrowave, Electropop]",
+    "gid": 8564,
+    "members_count": 28326
+  },
+  {
+    "name": "Depeche Mode",
+    "gid": 125927592,
+    "members_count": 154520
+  },
+  {
+    "name": "E:\\music\\trip-hop",
+    "gid": 27683540,
+    "members_count": 89928
+  }
+]
